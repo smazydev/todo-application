0bcafe779718565e74a04f7df5f1b9bf1e8263e8
smazydev <alizafar1437@gmail.com>
2020-11-15 00:26:52 +0500

    Added gradient background to application and made AddItemToList Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {AddItemToList} from './components/AddItemToList';
+import {ToDoListItem} from './components/ToDoListItem';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className="Title">Todo List Application</h1>
+      <AddItemToList/>
+      <ToDoListItem/>
     </div>
   );
 }
