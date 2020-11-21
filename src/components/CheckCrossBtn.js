8c51a61393866ad5317fb9dc021496e2af1ddea4
smazydev <alizafar1437@gmail.com>
2020-11-21 20:00:45 +0500

    Added simple completed function implementation

--- src/components/CheckCrossBtn.js
+++ src/components/CheckCrossBtn.js
@@ -3,14 +3,14 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCheckSquare,faWindowClose} from '@fortawesome/free-solid-svg-icons';
 import '../App.css';
 
-export const CheckCrossBtn = ({deleteHandler}) => {
+export const CheckCrossBtn = ({deleteHandler,completeHandler,todo}) => {
     const Check = <FontAwesomeIcon icon={faCheckSquare}/>
     const Cross = <FontAwesomeIcon icon={faWindowClose}/> 
 
 
     return (
         <>
-            <button className="Check ListBtn">{Check}</button>
+            <button onClick={completeHandler} className="Check ListBtn">{Check}</button>
             <button onClick={deleteHandler} className="Cross ListBtn">{Cross}</button>
         </>       
     )
--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -5,11 +5,20 @@ export const ToDoListItem = ({text,id,todo,todos,setTodos}) => {
     const deleteHandler = () => {
         setTodos(todos.filter((el)=> el.id !== todo.id));
     }
-    
+    const completeHandler = () => {
+        setTodos(todos.map(item => {
+            if (item.id === todo.id) {
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    }
     return (
-            <li>
+            <li className={`${todo.completed ? "completed" : ""}`}>
                 {text}
-                <CheckCrossBtn deleteHandler={deleteHandler}/>       
+                <CheckCrossBtn todo={todo} completeHandler={completeHandler} deleteHandler={deleteHandler}/>       
             </li>
     )
 }
